80b885ca9d65e248850b78e27b4b7f990f1b0902
LeoRoA <romanleontev@gmail.com>
2022-12-03 17:18:06 +0700

    Initial commit

---
+++ src/main/java/com/skypro/hw_maven/HwMavenApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.hw_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/hw_maven/HwMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.hw_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
